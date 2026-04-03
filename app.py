883ac57f6c743ec3b0f1268bdf4299404acff3b1
Roberta Goncalves <roberta.gf@outlook.com>
2026-04-03 14:08:27 +0200

    Add app.py & requirements.txt

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+# __name__ = "__main__" de forma manual 
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello world!'
+
+@app.route('/about')
+def about():
+    return 'Pagina sobre'
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
